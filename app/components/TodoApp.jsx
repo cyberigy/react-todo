cee8d2a6f9fbac3661083a543928eee223467227
Surasak P <surasak@Surasaks-Mac.local>
2017-02-07 16:43:14 +0700

    Add search component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,9 +1,13 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
+
 var TodoApp = React.createClass({
   getInitialState: function () {
     return  {
+      showCompleted: false,
+      searchText: '',
       todos: [
         {
           id: 1,
@@ -24,11 +28,18 @@ var TodoApp = React.createClass({
   handelAddTodo: function (text) {
     alert('new todo: ' + text);
   },
+  handleSearch: function (showCompleted,searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
   render: function() {
     var {todos} = this.state;
 
     return (
       <div>
+        <TodoSearch onSearch={this.handelSearch}/>
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handelAddTodo}/>
       </div>
